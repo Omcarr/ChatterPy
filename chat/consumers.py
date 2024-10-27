a5c7810a713e13014da4b4ae2d5b1b0c6a282715
Omkar <omkarnova@gmail.com>
2024-10-28 00:53:52 +0530

    added user online count

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from .models import ChatGroup, GroupMessage
 import json
+from django.contrib.auth.models import User
 
 class ChatroomConsumer(WebsocketConsumer):
     def connect(self):
@@ -14,13 +15,22 @@ class ChatroomConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_add)(
             self.chatroom_name, self.channel_name
         )
-
+        #add and update total online users of the group
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+        
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
         )
+        # remove and update online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count() 
+
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -49,3 +59,30 @@ class ChatroomConsumer(WebsocketConsumer):
         }
         html = render_to_string("chat/partials/chat_messages_p.html", context=context)
         self.send(text_data=html)
+    
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count()
+        
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+    
+
+    def online_count_handler(self, event):
+        online_count = event['online_count']-1 #not counting yourself as online member 
+        
+        # chat_messages = ChatGroup.objects.get(group_name=self.chatroom_name).chat_messages.all()[:30]
+        # author_ids = set([message.author.id for message in chat_messages])
+        # users = User.objects.filter(id__in=author_ids)
+        
+        context = {
+            'online_count' : online_count,
+            # 'chat_group' : self.chatroom,
+            # 'users': users
+        }
+        #print(online_count)
+        html = render_to_string("chat/partials/online_count.html", context)
+        self.send(text_data=html) 
